d5b794885f850b81acc916bd7999f04c6a9fc0cf
meharsg96 <mehar.grewal@mongodb.com>
2025-11-21 12:10:54 +0530

    next.config.mjs

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -2,10 +2,6 @@
 const nextConfig = {
   reactStrictMode: true,
   webpack(config) {
-    // Handle TypeScript files
-    config.resolve.extensionAlias = {
-      '.js': ['.js', '.jsx', '.ts', '.tsx'],
-    };
     // Polyfill Node.js core modules for browser environment
     config.resolve.fallback = {
       ...config.resolve.fallback,
